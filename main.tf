cb94d9a0ebabf197ae730a6ffb72db393c03ca3b
AnilNanda <anilthekkoote@gmil.com>
2021-06-03 20:34:28 +0530

    vpc creation using creds from github secrets

--- main.tf
+++ main.tf
@@ -1,10 +1,28 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "xxx"
-  secret_key = "yyy"
 }
 
-resource "aws_instance" "my-first-server" {
-  ami = "ami-0d5eff06f840b45e9"
-  instance_type = "t3.micro"
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.1.0.0/16"
+  instance_tenancy     = "default"
+  tags = {
+    Name      = "Vpc"
+    Terraform = "true"
+  }
+}
+# Build route table 1
+resource "aws_route_table" "route_table1" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "RouteTable1"
+    Terraform = "true"
+  }
+}
+# Build route table 2
+resource "aws_route_table" "route_table2" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "RouteTable2"
+    Terraform = "true"
+  }
 }
