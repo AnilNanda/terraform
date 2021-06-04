58c9428dbde63de339a787229eaabe7a60b5c8b2
AnilNanda <anilthekkoote@gmil.com>
2021-06-04 21:26:51 +0530

    Added ec2 instance

--- main.tf
+++ main.tf
@@ -11,6 +11,17 @@ resource "aws_vpc" "vpc" {
     Terraform = "true"
   }
 }
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.1.1.0/24"
+  availability_zone = "us-west-1a"
+
+  tags = {
+    Name = "subnet-1"
+  }
+}
+
 # Build route table 1
 resource "aws_route_table" "route_table1" {
   vpc_id = aws_vpc.vpc.id
@@ -25,5 +36,24 @@ resource "aws_route_table" "route_table2" {
   tags = {
     Name = "RouteTable2"
     Terraform = "true"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["10.1.1.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami = "ami-0d5eff06f840b45e9"
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index = 0
   }
 }
