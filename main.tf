355761e22324cdb9b755cd49ad000af14bee509f
Anil Nanda <anilthekkoote@gmail.com>
2022-04-01 14:34:29 +0530

    main.tf updated to add vpc module
    Added VPC module
    local variables defined
    Added multiple providers

--- main.tf
+++ main.tf
@@ -1,28 +1,46 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  profile = "default"
+  alias   = "virginia"
 }
+
+provider "aws" {
+  region  = "eu-central-1"
+  profile = "default"
+  alias   = "frankfurt"
+}
+
 terraform {
   backend "s3" {
-    bucket = "figopaul-terraform-out"
-    key = "terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "figopaul-terraform-out"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    profile = "default"
   }
 }
 
 #Define variable
 variable "ami_id" {
   description = "AMI id of webserver ec2"
-  default = "ami-0d5eff06f840b45e9"
-  type = string
+  default     = "ami-0d5eff06f840b45e9"
+  type        = string
+}
+
+#Define locals
+locals {
+  project_name = "fastapi"
+  billing      = "ABC"
 }
 
 #Create VPC
 resource "aws_vpc" "dev-vpc" {
-  cidr_block           = "10.1.0.0/16"
-  instance_tenancy     = "default"
+  provider         = aws.virginia
+  cidr_block       = "10.1.0.0/16"
+  instance_tenancy = "default"
   tags = {
     Name      = "Dev"
     Terraform = "true"
+    billing   = local.billing
   }
 }
 
@@ -31,19 +49,20 @@ resource "aws_internet_gateway" "dev-igw" {
   vpc_id = aws_vpc.dev-vpc.id
 
   tags = {
-    Name = "dev-igw"
+    Name    = "${local.project_name}-dev-igw"
+    billing = local.billing
   }
 }
 
 
 
 #Create Dev Subnets 1
-resource "aws_subnet" "dev-subnet-1"{
+resource "aws_subnet" "dev-subnet-1" {
   vpc_id            = aws_vpc.dev-vpc.id
   cidr_block        = "10.1.1.0/24"
   availability_zone = "us-east-1a"
   tags = {
-    Name = "Dev-subnet1"
+    Name = "${local.project_name}-dev-subnet1"
   }
 }
 
@@ -54,7 +73,7 @@ resource "aws_subnet" "dev-subnet-2" {
   availability_zone = "us-east-1b"
 
   tags = {
-    Name = "Dev-subnet2"
+    Name = "${local.project_name}-dev-subnet2"
   }
 }
 
@@ -68,7 +87,7 @@ resource "aws_route_table" "dev-rt" {
   }
 
   tags = {
-    Name = "dev-rt"
+    Name = "${local.project_name}-dev-rt"
   }
 }
 
@@ -80,34 +99,34 @@ resource "aws_route_table_association" "dev-rt-association" {
 #Create security group
 
 resource "aws_security_group" "webserver-sg" {
-  name        = "webserver-sg"
+  name        = "${local.project_name}-webserver-sg"
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.dev-vpc.id
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.dev-vpc.cidr_block]
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.dev-vpc.cidr_block]
   }
   ingress {
-    description      = "SSH from my ip"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["111.92.44.137/32"]
+    description = "SSH from my ip"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["111.92.44.137/32"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "${local.project_name}-allow_tls"
   }
 }
 
@@ -124,8 +143,8 @@ resource "aws_eip" "web-eip" {
 }
 
 resource "aws_network_interface" "web-interface" {
-  subnet_id   = aws_subnet.dev-subnet-1.id
-  private_ips = ["10.1.1.100"]
+  subnet_id       = aws_subnet.dev-subnet-1.id
+  private_ips     = ["10.1.1.100"]
   security_groups = [aws_security_group.webserver-sg.id]
   tags = {
     Name = "webserver_network_interface"
@@ -133,20 +152,43 @@ resource "aws_network_interface" "web-interface" {
 }
 
 resource "aws_instance" "web-server" {
-  ami = var.ami_id
-  instance_type = "t2.micro"
-  key_name = "figopaul587"
+  ami               = var.ami_id
+  instance_type     = "t2.micro"
+  key_name          = "figopaul587"
   availability_zone = "us-east-1a"
   network_interface {
     network_interface_id = aws_network_interface.web-interface.id
-    device_index = 0
+    device_index         = 0
   }
   tags = {
     Name = "webserver"
+    Type = "EC2"
   }
   depends_on = [aws_eip.web-eip]
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  providers = {
+    aws = aws.frankfurt
+   }
+
+  name = "dev-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 output "ec2_public_ip" {
-  value = aws_eip.web-eip.public_ip
+  value = aws_instance.web-server.public_ip
 }
