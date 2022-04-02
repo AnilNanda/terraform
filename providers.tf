f277f1f7fccf7d155c027e4f137deb0a6e00ad1a
Anil Nanda <anilthekkoote@gmail.com>
2022-04-02 10:43:13 +0530

    terraform remote provider
    Using terraform cloud to store the state file

--- providers.tf
+++ providers.tf
@@ -1,20 +1,31 @@
 provider "aws" {
   region  = "us-east-1"
-  profile = "default"
   alias   = "virginia"
 }
 
 provider "aws" {
   region  = "eu-central-1"
-  profile = "default"
   alias   = "frankfurt"
 }
 
 terraform {
-  backend "s3" {
-    bucket  = "figopaul-terraform-out"
-    key     = "terraform.tfstate"
-    region  = "us-east-1"
-    profile = "default"
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.8.0"
+    }
   }
+    backend "remote" {
+      hostname = "app.terraform.io"
+      organization = "anilnanda"
+      workspaces {
+        name = "dev"
+      }
+    }
+#   backend "s3" {
+#     bucket  = "figopaul-terraform-out"
+#     key     = "terraform.tfstate"
+#     region  = "us-east-1"
+#     profile = "default"
+#   }
 }
