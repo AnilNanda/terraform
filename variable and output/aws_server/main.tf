e34ddf68363ccc2eeba96a461627c5e840194943
Anil Nanda <anilthekkoote@gmail.com>
2022-04-05 07:24:10 +0530

    data to filter resources
    Using data to get the AWS AMI details or any other resource details using filter

--- variable and output/aws_server/main.tf
+++ variable and output/aws_server/main.tf
@@ -23,8 +23,22 @@ variable "instance_type" {
   }
 }
 
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    filter {
+        name = "name"
+        values= ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"]
+}
+
 resource "aws_instance" "web" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   key_name               = "figopaul587"
   tags = {
